c49ae1a254e983b86bc0e56440d94148f57e43d6
twinkle-tyagi <twinkletyagi@niet.co.in>
2022-10-28 15:04:58 +0530

    basics

---
+++ app.ts
@@ -0,0 +1,90 @@
+//HTMLInputElement, HTMLButtonElement are general html DOM type present in TS
+
+//const num1Element = document.getElementById('num1'); //tell this is input type
+const num1Element = document.getElementById('num1') as HTMLInputElement; // type casting.
+const num2Element = document.getElementById('num2') as HTMLInputElement;
+const btn = document.querySelector('button')!; // it is queried and selected as button, so its type is known.
+// we add ! at end of button because we know for sure that it is a button and we want TS ignore checking it.
+
+/*
+function add(a: number, b: number) { //if you dont know type of variable you can set it to type "any"
+    return a + b;
+}
+*/
+
+//creating array - array is generic type, outer type encloses values inside, that have their own type.
+
+//const numResults = []; // we need to specify type of array we are creating
+//const textResults = [];
+
+//const numResults: Array<number> = []; // we define array like this
+//shortcut to define array -
+const numResults: number[] = [];    // number[] is called array type.
+const textResults: string[] = [];  // string[] is called array type specify that it is array of string
+
+type numOrString = number|string;  // creates our own type numOrString 
+type Result = {val: number, timestamp: Date};
+
+//alternative is interfaces, which allows us to define sturcture of an object.
+interface ResultObject {
+    val: number, 
+    timestamp: Date
+};
+
+//function add(num1: number | string, num2: number | string) {
+// called union type. a can be of type number or string.
+
+function add(num1: numOrString, num2: numOrString) {
+// we need to do different operation based on type. so we use if-else condition code called type guard.
+    if(typeof(num1) === 'number' && typeof(num2) === 'number') {
+        return num1 + num2;
+    }
+    else if(typeof(num1) === 'string' && typeof(num2) === 'string') {
+        return num1 + "" + num2;
+    }
+    else {
+        return +num1 + +num2; //convert to number if any other type
+    }
+    
+}
+// objects
+function printResult(resultObj: Result) { //{ } defines structure of object, with parameters and thier type
+    console.log(resultObj.val)
+}
+
+
+
+btn.addEventListener('click' , () => {
+    const num1 = num1Element.value; // will give error
+// because we can only get value of input types and TS dont know type of num1Element. so we have to tell it
+    const num2 = num2Element.value;
+
+    //const result = add(num1, num2); // .value returns a string so it will give error
+    // we need to convert it to number
+    const result = add(+num1, +num2);
+    console.log(result);
+    numResults.push(result as number);
+
+    const stringResult = add(num1, num2);
+    console.log(stringResult);
+    textResults.push(stringResult as string);
+
+    printResult({val: result as number, timestamp: new Date()}); //type casting - casting result as number
+
+    console.log(numResults);    // shows error that type of array is any.
+    console.log(textResults);
+});
+
+//const myPromise = new Promise((resolve, reject) => {
+const myPromise = new Promise<string>((resolve, reject) => { //to specify which type it will resolve
+    setTimeout(() => {
+        resolve("It worked"); // here is resolves a string, so type of promise is string
+    }, 1000);
+})
+
+myPromise
+.then(res => {
+    console.log(res);
+    console.log(res.split("w")); // as generic type of promise is string, now we can call string methods on res
+})
+.catch(err => console.log(err));
